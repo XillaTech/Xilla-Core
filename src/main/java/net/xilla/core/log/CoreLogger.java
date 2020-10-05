8297992b59ad1677083fa1a5c63c3f1fc3cd03b2
Tobias Steely <Tobias.steely@gmail.com>
2020-10-05 16:09:55 -0400

    Upgrade Logger

--- src/main/java/net/xilla/core/log/CoreLogger.java
+++ src/main/java/net/xilla/core/log/CoreLogger.java
@@ -9,11 +9,17 @@ public class CoreLogger implements Log {
     @Override
     public void log(LogLevel level, String message, Class clazz) {
         if(level.level >= Logger.getLogLevel().level) {
-            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss ");
             df.setTimeZone(Logger.getTimeZone());
             String nowAsISO = df.format(new Date());
 
-            String log = "[" + nowAsISO + "] [" + level.name() + "] (" + clazz.getName() + ") " + message;
+            String log;
+            if(level == LogLevel.INFO) {
+                log = "[" + nowAsISO + " " + Logger.getTimeZone().getDisplayName() + " " + level.name() + "] " + message;
+            } else {
+                log = "[" + nowAsISO + " " + Logger.getTimeZone().getDisplayName() + " " + level.name() + "] (" + clazz.getName() + ") " + message;
+            }
+
             System.out.println(log);
         }
     }
@@ -21,16 +27,16 @@ public class CoreLogger implements Log {
     @Override
     public void log(LogLevel level, Throwable message, Class clazz) {
         if(level.level >= Logger.getLogLevel().level) {
-            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+            DateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss ");
             df.setTimeZone(Logger.getTimeZone());
             String nowAsISO = df.format(new Date());
 
-            String log = "[" + nowAsISO + "] [" + level.name() + "] (" + clazz.getName() + ") " + message.toString();
+            String log = "[" + nowAsISO + " " + Logger.getTimeZone().getDisplayName() + " " + level.name() + "] (" + clazz.getName() + ") " + message.toString();
 
             System.out.println(log);
 
             for(int i = 0; i < message.getStackTrace().length; i++) {
-                log = "[" + nowAsISO + "] [" + level.name() + "] (" + clazz.getName() + ") [ERR] " + message.getStackTrace()[i].toString();
+                log = "[" + nowAsISO + " " + Logger.getTimeZone().getDisplayName() + " " + level.name() + "] (" + clazz.getName() + ") [ERR] " + message.getStackTrace()[i].toString();
                 System.out.println(log);
             }
         }
